cda6ec834106058bba2b8a6e3d653b91640c197b
saisrijena <sairam.jena001@gmail.com>
2026-06-03 15:55:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-api_key = st.secrets["GEMINI_API_KEY"]
+import streamlit as st
+import fitz
+from docx import Document
+from google import genai
+import pandas as pd
+import io
+import json
+
+api_key = st.secrets["AQ.Ab8RN6KMwmej5CJPC8cpm68_qaXzF6X8GpLXkBv7M1J_J1xXHw"]
